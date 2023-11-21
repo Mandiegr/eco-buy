b6c4633b69b2e67cf5b9332b634d813dc74be482
Mandiegr <nannda076@gmail.com>
2023-11-21 00:32:14 -0300

    file productPage

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,30 @@
-'use-client'
 import React from 'react';
+import Product from "../products/data/ProductList";
+import { Header } from '../../../components/Header';
 
- function ProductPage (){
- 
+const ProductsPage = () => {
   return (
-    <div>
-      <h1>Produtos</h1>
-    </div>
+    <section>
+    <Header/>
+     <div style={{ flex: 1 , backgroundColor:' #fff', width:'100%'}}>   {/*#ffe5aa*/}
+       
+       
+       <div style={{ display: 'flex',flexDirection:'row', justifyContent:'center', padding:'30px'}}>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+       </div>
+      
+      </div>
+
+    </section> 
+   
   );
 };
 
-export default ProductPage 
+
+
+export default ProductsPage;
