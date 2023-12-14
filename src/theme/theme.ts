94f15ab67d1ab73c18dbca9f679c4c8184490829
Mandiegr <nannda076@gmail.com>
2023-12-14 15:47:00 -0300

    feat: add theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,13 @@
+const Colors = {
+    white: '#fff',
+    black: 'black',
+    gray: 'gray',
+
+    bege: '#e4e0c0',
+    green100: '#304918',
+    green200: '#556b2f',
+    green300: '#3d7737',
+    
+  };
+  
+  export default Colors;
